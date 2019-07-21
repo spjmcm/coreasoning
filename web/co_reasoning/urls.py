67d7b373784235ad607962662f5aa2952e93bfc7
roryzhengzhang <roryzhang95@gmail.com>
2019-07-21 15:01:17 -0400

    Update urls.py

--- web/co_reasoning/urls.py
+++ web/co_reasoning/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('', views.generalize, name='generalize'),
+    path('generalize', views.generalize, name='generalize'),
     url(r'^get_dropped_data/', views.get_dropped_data, name='get_dropped')
 ]
