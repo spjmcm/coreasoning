270e87f2988cbd9b6165b605d4c46b84eeef2b1a
roryzhengzhang <roryzhang95@gmail.com>
2019-08-16 15:13:21 -0400

    update SQLWriter.py

--- web_django/web/SQLWriter.py
+++ web_django/web/SQLWriter.py
@@ -1,4 +1,4 @@
-import mysql.connector
+iimport mysql.connector
 from mysql.connector import Error
 from mysql.connector import errorcode
 import pandas as pd
@@ -14,8 +14,21 @@ def insertRecord(group_name, story, question1, question2):
                                              user='zhengzhang',
                                              password='zz498270958')
         cursor = connection.cursor(prepared=True)
+        sql_create_table = '''CREATE TABLE IF NOT EXISTS stories (
+        group_name VARCHAR(255),
+        sen_1 VARCHAR(255),
+        sen_2 VARCHAR(255),
+        sen_3 VARCHAR(255),
+        sen_4 VARCHAR(255),
+        sen_5 VARCHAR(255),
+        q1 VARCHAR(255),
+        q2 VARCHAR(255),
+        story_id int(11) not null auto_increment,
+        primary key (story_id)
+        )'''
         sql_insert_query = ''' INSERT INTO stories (group_name, sen_1, sen_2, sen_3, sen_4, sen_5, Q1, Q2) VALUES (%s, %s, %s, %s, %s, %s, %s)'''
         insert_tuple = (group_name, sentenses[0].strip(), sentenses[1].strip(), sentenses[2].strip(), sentenses[3].strip(), sentenses[4].strip(), question1, question2)
+        setup_table = cursor.execute(sql_create_table)
         result = cursor.execute(sql_insert_query, insert_tuple)
         connection.commit()
         print("Record inserted successfully into stories table")
