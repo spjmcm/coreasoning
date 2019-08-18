9991320f3c5d2162e7cba4568fbea6d50164151a
roryzhengzhang <roryzhang95@gmail.com>
2019-08-18 16:03:00 -0400

    Update addRule.js

--- web_django/co_reasoning/static/js/addRule.js
+++ web_django/co_reasoning/static/js/addRule.js
@@ -3,6 +3,6 @@ $('.add-rule-button').on('click', () => {
   verb = $('#verb').val()
   object = $('#object').val();
   new_rule = verb+'('+subject+','+object+')';
-  $('#list-narrative').append("<li class='narrative nav-item' draggable='true' ondragstart='drag(event)' id='drag" + index + "'>" +
+  $('#list-narrative').append("<li class='narrative nav-item' draggable='true' ondragstart='drag(event)' id='drag" + narrative_count+1 + "'>" +
       "<i class='fa fa-circle-o'></i> " + new_rule + "</li>");
 })
