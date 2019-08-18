481deb77eaba0c2f9cac6603db9cacab79277387
roryzhengzhang <roryzhang95@gmail.com>
2019-08-18 16:56:48 -0400

    Update addRule.js

--- web_django/co_reasoning/static/js/addRule.js
+++ web_django/co_reasoning/static/js/addRule.js
@@ -6,6 +6,7 @@ $('.add-rule-button').on('click', () => {
   has_contain = false;
   $('#list-narrative').each(function(index) {
      console.log($(this).text());
+     console.log($(this).text() == new_rule);
     if($(this).text() == new_rule)
     {
       console.log('new rule has existed');
