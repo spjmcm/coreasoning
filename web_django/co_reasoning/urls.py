b2d67948d47f2263d2a25b62ec93dded99906402
roryzhengzhang <roryzhang95@gmail.com>
2019-08-19 14:38:58 -0400

    Update urls.py

--- web_django/co_reasoning/urls.py
+++ web_django/co_reasoning/urls.py
@@ -5,6 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('generalize', views.generalize, name='generalize'),
-    path('wordreplacement', views.wordreplacement, name="wordreplacement")
+    path('wordreplacement', views.wordreplacement, name="wordreplacement"),
     url(r'^get_dropped_data/', views.get_dropped_data, name='get_dropped')
 ]
