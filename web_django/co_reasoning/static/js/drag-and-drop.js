cd4549e43a85fc57f058b8849a473e337a3753f7
roryzhengzhang <roryzhang95@gmail.com>
2019-08-19 14:31:45 -0400

    Update drag-and-drop.js

--- web_django/co_reasoning/static/js/drag-and-drop.js
+++ web_django/co_reasoning/static/js/drag-and-drop.js
@@ -29,7 +29,7 @@ $(document).ready(function () {
     });
 
     $('#submit').click(function (){
-      window.location.href = "wordreplacement.html";
+      window.location.href = "/templates/wordreplacement.html";
       return false;
     })
 });
