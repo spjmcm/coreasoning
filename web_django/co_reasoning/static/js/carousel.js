dfe49d6da62b3fce3df21fc9b72a792de0395cfe
roryzhengzhang <roryzhang95@gmail.com>
2019-08-19 17:30:58 -0400

    Add prev and next button

--- web_django/co_reasoning/static/js/carousel.js
+++ web_django/co_reasoning/static/js/carousel.js
@@ -1,4 +1,10 @@
 $(document).ready(function () {
     // disable automatic carousel
     $('.carousel').carousel({interval: false});
+    $('#prev-button').on('click', function() {
+      $('.carousel').carousel('prev');
+    });
+    $('#next-button').on('click', function() {
+      $('.carousel').carousel('next');
+    });
 });
