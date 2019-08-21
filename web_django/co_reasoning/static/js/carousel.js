102216b043cdc187d5ef0f0fed7f7ada7e5b5950
roryzhengzhang <roryzhang95@gmail.com>
2019-08-21 12:00:47 -0400

    Change the rule list

--- web_django/co_reasoning/static/js/carousel.js
+++ web_django/co_reasoning/static/js/carousel.js
@@ -7,4 +7,17 @@ $(document).ready(function () {
     $('.next-button').on('click', function() {
       $('.carousel').carousel('next');
     });
+
+    initRuleList();
 });
+
+function initRuleList(){
+  //get the length of json array
+  data = $.getJSON("/static/data/rules.json");
+  len = data.length();
+  for(var i=0; i<data.length; i++)
+  {
+    $('#list-narrative').append("<li class='narrative nav-item' id='rule" + (i+1) + "'>" +
+        "<i class='fa fa-circle-o'></i> " + rule+(i+1) +"</li>")
+  }
+}
